6cc25ed95d8726390c5650e469d2b676ad35c946
Ian Perez <iperez@mit.edu>
2022-11-22 17:13:04 -0500

    Implemented login/registration page

--- client/components/common/BlockForm.vue
+++ client/components/common/BlockForm.vue
@@ -2,7 +2,7 @@
 <!-- This is just an example; feel free to define any reusable components you want! -->
 
 <template>
-  <form @submit.prevent="submit">
+  <b-form @submit.prevent="submit">
     <h3>{{ title }}</h3>
     <article
       v-if="fields.length"
@@ -11,30 +11,31 @@
         v-for="field in fields"
         :key="field.id"
       >
-        <label :for="field.id">{{ field.label }}:</label>
-        <textarea
+        <label :for="field.id" v-if="field.label != 'Content'">{{ field.label }}:</label>
+        <b-form-textarea
           v-if="field.id === 'content'"
           :name="field.id"
           :value="field.value"
           @input="field.value = $event.target.value"
         />
-        <input
+        <b-input
           v-else
           :type="field.id === 'password' ? 'password' : 'text'"
           :name="field.id"
           :value="field.value"
-          @input="field.value = $event.target.value"
-        >
+          @input="field.value = $event"
+        />
       </div>
     </article>
     <article v-else>
       <p>{{ content }}</p>
     </article>
-    <button
+    <b-button
+      variant="primary"
       type="submit"
     >
       {{ title }}
-    </button>
+    </b-button>
     <section class="alerts">
       <article
         v-for="(status, alert, index) in alerts"
@@ -44,108 +45,109 @@
         <p>{{ alert }}</p>
       </article>
     </section>
-  </form>
+  </b-form>
 </template>
 
 <script>
 
 export default {
-  name: 'BlockForm',
-  data() {
+name: 'BlockForm',
+data() {
+  /**
+   * Options for submitting this form.
+   */
+  return {
+    url: '', // Url to submit form to
+    method: 'GET', // Form request method
+    hasBody: false, // Whether or not form request has a body
+    setUsername: false, // Whether or not stored username should be updated after form submission
+    refreshFreets: false, // Whether or not stored freets should be updated after form submission
+    alerts: {}, // Displays success/error messages encountered during form submission
+    callback: null // Function to run after successful form submission
+  };
+},
+methods: {
+  async submit() {
     /**
-     * Options for submitting this form.
-     */
-    return {
-      url: '', // Url to submit form to
-      method: 'GET', // Form request method
-      hasBody: false, // Whether or not form request has a body
-      setUsername: false, // Whether or not stored username should be updated after form submission
-      refreshFreets: false, // Whether or not stored freets should be updated after form submission
-      alerts: {}, // Displays success/error messages encountered during form submission
-      callback: null // Function to run after successful form submission
+      * Submits a form with the specified options from data().
+      */
+    const options = {
+      method: this.method,
+      headers: {'Content-Type': 'application/json'},
+      credentials: 'same-origin' // Sends express-session credentials with request
     };
-  },
-  methods: {
-    async submit() {
-      /**
-        * Submits a form with the specified options from data().
-        */
-      const options = {
-        method: this.method,
-        headers: {'Content-Type': 'application/json'},
-        credentials: 'same-origin' // Sends express-session credentials with request
-      };
-      if (this.hasBody) {
-        options.body = JSON.stringify(Object.fromEntries(
-          this.fields.map(field => {
-            const {id, value} = field;
-            field.value = '';
-            return [id, value];
-          })
-        ));
+    if (this.hasBody) {
+      options.body = JSON.stringify(Object.fromEntries(
+        this.fields.map(field => {
+          const {id, value} = field;
+          field.value = '';
+          return [id, value];
+        })
+      ));
+    }
+
+    try {
+      const r = await fetch(this.url, options);
+      if (!r.ok) {
+        // If response is not okay, we throw an error and enter the catch block
+        const res = await r.json();
+        throw new Error(res.error);
       }
 
-      try {
-        const r = await fetch(this.url, options);
-        if (!r.ok) {
-          // If response is not okay, we throw an error and enter the catch block
-          const res = await r.json();
-          throw new Error(res.error);
-        }
-
-        if (this.setUsername) {
-          const text = await r.text();
-          const res = text ? JSON.parse(text) : {user: null};
-          this.$store.commit('setUsername', res.user ? res.user.username : null);
-        }
-
-        if (this.refreshFreets) {
-          this.$store.commit('refreshFreets');
-        }
-
-        if (this.callback) {
-          this.callback();
-        }
-      } catch (e) {
-        this.$set(this.alerts, e, 'error');
-        setTimeout(() => this.$delete(this.alerts, e), 3000);
+      if (this.setUsername) {
+        const text = await r.text();
+        const res = text ? JSON.parse(text) : {user: null};
+        this.$store.commit('setUsername', res.user ? res.user.username : null);
       }
+
+      if (this.refreshFreets) {
+        this.$store.commit('refreshFreets');
+      }
+
+      if (this.callback) {
+        this.callback();
+      }
+    } catch (e) {
+      this.$set(this.alerts, e, 'error');
+      setTimeout(() => this.$delete(this.alerts, e), 3000);
     }
   }
+}
 };
 </script>
 
 <style scoped>
 form {
-  border: 1px solid #111;
-  padding: 0.5rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  margin-bottom: 14px;
-  position: relative;
+border: 1px solid rgb(99, 99, 99);
+padding: 0.5rem;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+margin-bottom: 14px;
+position: relative;
+border-radius: 5px;
 }
 
 article > div {
-  display: flex;
-  flex-direction: column;
+display: flex;
+flex-direction: column;
 }
 
 form > article p {
-  margin: 0;
+margin: 0;
 }
 
 form h3,
 form > * {
-  margin: 0.3em 0;
+margin: 0.3em 0;
 }
 
 form h3 {
-  margin-top: 0;
+margin-top: 0;
 }
 
 textarea {
-   font-family: inherit;
-   font-size: inherit;
+ font-family: inherit;
+ font-size: inherit;
 }
 </style>
---
+++ client/home.vue
@@ -0,0 +1,58 @@
+<!-- Default page that also displays freets -->
+
+<template>
+  <main>
+    <section v-if="$store.state.username">
+      <header>
+        <h2>Welcome @{{ $store.state.username }}</h2>
+      </header>
+      <CreateFreetForm />
+    </section>
+    <section v-else>
+      <header>
+        <h2>Welcome to Automech!</h2>
+      </header>
+      <article>
+        <h3>
+          Please 
+          <router-link to="/login">
+            Sign in or create an account
+          </router-link>
+        </h3>
+      </article>
+    </section>
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: 'FreetPage',
+  mounted() {
+    this.$refs.getFreetsForm.submit();
+  }
+};
+</script>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+}
+
+header, header > * {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+button {
+    margin-right: 10px;
+}
+
+section .scrollbox {
+  flex: 1 0 50vh;
+  padding: 3%;
+  overflow-y: scroll;
+}
+</style>
