ab70f764253ba822149c589a532f61151b109d32
Jacob Cremers <jacob.am.cremers@gmail.com>
2022-12-04 12:57:05 -0500

    Fixed account creation bug

--- server/user/router.ts
+++ server/user/router.ts
@@ -105,7 +105,7 @@ router.post(
     userValidator.isValidPassword
   ],
   async (req: Request, res: Response) => {
-    const user = await UserCollection.addOne(req.body.username, req.body.password);
+    const user = await UserCollection.addOne(req.body.username, req.body.password, req.body.name);
     req.session.userId = user._id.toString();
     res.status(201).json({
       message: `Your account was created successfully. You have been logged in as ${user.username}`,
