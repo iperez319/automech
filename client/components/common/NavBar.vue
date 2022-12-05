2d4b626aa8c6debd476c40fc24f545c863f67891
Ian Perez <iperez@mit.edu>
2022-12-04 22:50:20 -0500

    Added compare to navbar

--- client/components/common/NavBar.vue
+++ client/components/common/NavBar.vue
@@ -38,46 +38,34 @@
     </section>
   </nav> -->
   <div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand to="/" class="brand">
-      <h1 class="title">
-        Automech
-      </h1>
-    </b-navbar-brand>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand to="/" class="brand">
+        <h1 class="title">Automech</h1>
+      </b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item to="/">Home</b-nav-item>
-        <b-nav-item to="/explore">Explore</b-nav-item>
-        <b-nav-item to="/review">Review</b-nav-item>
-        <b-nav-item
-          v-if="$store.state.username"
-          to="/account"
-        >
-        Account
-        </b-nav-item>
-        <b-nav-item
-          v-else
-          to="/login"
-        >
-        Log in
-        </b-nav-item>
-        
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/compare">Compare</b-nav-item>
+          <b-nav-item to="/explore">Explore</b-nav-item>
+          <b-nav-item to="/review">Review</b-nav-item>
+          <b-nav-item v-if="$store.state.username" to="/account">
+            Account
+          </b-nav-item>
+          <b-nav-item v-else to="/login"> Log in </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <style scoped>
-
 .title {
-    font-size: 32px;
-    margin: 0 5px;
+  font-size: 32px;
+  margin: 0 5px;
 }
 
 .brand {
@@ -87,27 +75,27 @@
 }
 
 img {
-    height: 32px;
+  height: 32px;
 }
 
 .left {
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 }
 
 .right {
-    font-size: 20px;
-    display: grid;
-    gap: 16px;
-    grid-auto-flow: column;
-    align-items: center;
+  font-size: 20px;
+  display: grid;
+  gap: 16px;
+  grid-auto-flow: column;
+  align-items: center;
 }
 
 .right a {
-    margin-left: 5px;
+  margin-left: 5px;
 }
 
 .alerts {
-    width: 25%;
+  width: 25%;
 }
 </style>
