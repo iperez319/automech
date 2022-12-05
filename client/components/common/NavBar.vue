02b3cd3e90f0fbf89d51a83edb98d84e05556692
Jacob Cremers <jacob.am.cremers@gmail.com>
2022-12-04 22:43:29 -0500

    Super difficult change, made explore page home page

--- client/components/common/NavBar.vue
+++ client/components/common/NavBar.vue
@@ -51,8 +51,7 @@
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-item to="/">Home</b-nav-item>
-        <b-nav-item to="/explore">Explore</b-nav-item>
+        <b-nav-item to="/explore">Home</b-nav-item>
         <b-nav-item to="/review">Review</b-nav-item>
         <b-nav-item
           v-if="$store.state.username"
